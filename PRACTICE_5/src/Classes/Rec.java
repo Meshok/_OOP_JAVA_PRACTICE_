70ac6282a020359743c3200e3de282f046561aeb
Meshok <34945779+Meshok@users.noreply.github.com>
2022-09-20 19:04:47 +0300

    Practice 5 v0.1

--- PRACTICE_5/src/Classes/Rec.java
+++ PRACTICE_5/src/Classes/Rec.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 
 public class Rec {
     static public int max() {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        int a = new Scanner(System.in).nextInt();
         int res;
         if (a == 0) {
             res = 0;
