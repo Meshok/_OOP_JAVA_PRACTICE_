387b9e73dd3e148c2b70ced563564752d014f018
Meshok <34945779+Meshok@users.noreply.github.com>
2022-09-23 18:04:06 +0300

    Practice 6 v0.5

--- PRACTICE_6/src/Main.java
+++ PRACTICE_6/src/Main.java
@@ -1,18 +1,17 @@
 import Classes.*;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Student[] array = {
-                new Student(1, 55),
-                new Student(2, 22),
-                new Student(3, 25)};
+            new Student(1, 55),
+            new Student(2, 22),
+            new Student(3, 25)
+        };
         for (Student it: array) {
             System.out.println(it);
         }
 
-        Arrays.sort(array, new SortingStudentsByGPA());
+        SortingAlgorithms.quickSort(array, new SortingStudentsByGPA());
 
         System.out.println("===========================");
         for (Student it: array) {
