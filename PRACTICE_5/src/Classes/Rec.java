3c3e37649854a5eeadf7e7eb7515c3250cb2ff89
Meshok <34945779+Meshok@users.noreply.github.com>
2022-10-24 02:24:27 +0300

    Practice 5 v0.6

--- PRACTICE_5/src/Classes/Rec.java
+++ PRACTICE_5/src/Classes/Rec.java
@@ -1,5 +1,4 @@
 package Classes;
-
 import java.util.Scanner;
 
 public class Rec {
@@ -10,8 +9,26 @@ public class Rec {
             res = 0;
         } else {
             res = max();
-            res = res < a ? a : res;
+            res = Math.max(res, a);
         }
         return res;
+    }
+
+    static public void triangleSequence(int n) {
+        if (n > 0) fu(n, 0);
+    }
+
+    static private void fu(int n, int k) {
+        if (n > 0) {
+            for (int i = 0; i < Math.min(n, k); i++) System.out.print(k + " ");
+            fu(n - k, k + 1);
+        }
+    }
+
+    static public void numSequence(int n) {
+        if (n > 0) {
+            numSequence(n - 1);
+            System.out.print(n + " ");
+        }
     }
 }
--- PRACTICE_5/src/Main.java
+++ PRACTICE_5/src/Main.java
@@ -2,6 +2,8 @@ import Classes.Rec;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Rec.max());
+//         System.out.println(Rec.max());
+//         Rec.triangleSequence(10);
+//         Rec.numSequence(67);
     }
 }
