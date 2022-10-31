1c3d02312771e86e26a3738f00b3141e9e8afff9
Meshok <34945779+Meshok@users.noreply.github.com>
2022-10-31 13:53:09 +0300

    fix repo

---
+++ LAB_6/src/Main.java
@@ -0,0 +1,8 @@
+import Classes.Program;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Program();
+    }
+}
---
+++ PRACTICE_7/src/Main.java
@@ -0,0 +1,8 @@
+import Classes.Game;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Game();
+    }
+}
